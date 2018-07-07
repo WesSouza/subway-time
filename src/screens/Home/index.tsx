7be281f4f295cc5c764dca9a5b318ea6cf7e4e53
Wesley de Souza <wesleywex@gmail.com>
2018-07-06 20:06:56 -0400

    Add signature.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,9 +8,19 @@ import styles from './styles.css';
 
 const Home = ({ path }: IProps) => (
   <div className={styles.Home}>
-    Welcome.<br />
-    <br />
-    Begin by searching for a station above.
+    <p>Welcome.</p>
+    <p>Begin by searching for a station above.</p>
+    <p className={`${styles.footnote} ${styles.smaller}`}>
+      Built with <span className={styles.love}>&lt;3</span> by{' '}
+      <a href="https://wesley.so/" target="_blank">
+        @WesleydeSouza
+      </a>.
+    </p>
+    <p className={styles.smaller}>
+      <a href="https://github.com/WesleydeSouza/subway-time" target="_blank">
+        Fork it on GitHub.
+      </a>
+    </p>
   </div>
 );
 
