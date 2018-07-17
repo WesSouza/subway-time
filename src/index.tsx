af79df3f48013c028506fe75aa213422cdd6b4a0
Wesley de Souza <wesleywex@gmail.com>
2018-07-16 21:45:00 -0400

    Let's not redirect localhost.

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 
 import './global.css';
 
-if (location.protocol !== 'https:') {
+if (location.protocol !== 'https:' && location.hostname !== 'localhost') {
   location.href = location.href.replace(/^http:\/\//, 'https://');
 } else {
   ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
