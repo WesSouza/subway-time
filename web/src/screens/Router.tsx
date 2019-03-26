00d1e73434fd94691a293828cd06d3263c5f49e9
Wes Souza <wesleydesouza@me.com>
2019-03-26 00:11:24 -0400

    Remove unused <Map />

--- web/src/screens/Router.tsx
+++ web/src/screens/Router.tsx
@@ -2,14 +2,12 @@ import { Router } from '@reach/router';
 import * as React from 'react';
 
 import Home from '~/screens/Home';
-import Map from '~/screens/Map';
 import NotFound from '~/screens/NotFound';
 import Station from '~/screens/Station';
 
 const AppRouter = () => (
   <Router>
     <Home path="/" />
-    <Map path="/map" />
     <Station path="/station/:stationId" />
     <NotFound default={true} />
   </Router>
