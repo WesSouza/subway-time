195f13df8b7312475a189932359639b291ee9072
Wes Souza <wesleydesouza@me.com>
2019-04-02 23:13:54 -0400

    Add components LinedBlock and LineIcon

---
+++ web/src/components/LinedBlock/index.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode } from 'react';
+
+import { LineIcon } from '../LineIcon';
+
+import styles from './styles.css';
+
+interface IProps {
+  children?: ReactNode;
+  icon?: ReactNode;
+  lineId?: string;
+  subtitle?: ReactNode;
+  title?: ReactNode;
+}
+
+export const LinedBlock = ({
+  children,
+  icon,
+  lineId,
+  subtitle,
+  title,
+}: IProps) => {
+  return (
+    <div className={styles.LinedBlock}>
+      <div className={styles.line}>
+        {lineId || icon ? (
+          <div className={styles.icon}>
+            {lineId ? <LineIcon>{lineId}</LineIcon> : icon}
+          </div>
+        ) : null}
+      </div>
+      <div
+        className={`${styles.content} ${
+          lineId && title ? styles.alignLineIdTitle : ''
+        }`}
+      >
+        {title && <h1 className={styles.title}>{title}</h1>}
+        {subtitle && <div className={styles.subtitle}>{subtitle}</div>}
+        {children && <div className={styles.children}>{children}</div>}
+      </div>
+    </div>
+  );
+};
