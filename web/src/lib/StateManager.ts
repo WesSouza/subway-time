8963ad1744210432cf61f77a7028f70fa6bad83e
Wes Souza <wesleydesouza@me.com>
2019-10-21 16:05:31 -0400

    Add immer and new StateManager

---
+++ web/src/lib/StateManager.ts
@@ -0,0 +1,98 @@
+import produce, { Draft } from 'immer';
+import { useEffect, useState, useRef } from 'react';
+
+export type SelectorFn<T, U> = (state: T) => U;
+
+interface Selector<T, U> {
+  callback: (newValue: U) => void;
+  currentValue: U;
+  selectorFn: SelectorFn<T, U>;
+}
+
+export class StateManager<T> {
+  private internalData: Readonly<T>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private publishTimer: any = null;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private subscribers = new Map<(newValue: any) => void, Selector<T, any>>();
+
+  constructor(initialState: T) {
+    this.internalData = initialState;
+  }
+
+  get state(): Readonly<T> {
+    return this.internalData;
+  }
+
+  mutate(mutation: (draft: Draft<T>) => void) {
+    this.internalData = produce(this.internalData, mutation);
+
+    // Delay publishing to the next macro task
+    clearTimeout(this.publishTimer);
+    this.publishTimer = setTimeout(() => {
+      this.publish();
+    }, 0);
+  }
+
+  publish() {
+    for (const [callback, selector] of this.subscribers) {
+      const { currentValue, selectorFn } = selector;
+      const newValue = selectorFn(this.internalData);
+      if (newValue === currentValue) {
+        continue;
+      }
+
+      this.subscribers.set(callback, {
+        ...selector,
+        currentValue: newValue,
+      });
+
+      // TODO: protect against infinite recursion by incrementing and
+      // decrementing a counter around this call
+      callback.call(null, newValue);
+    }
+  }
+
+  subscribe<U>(selectorFn: SelectorFn<T, U>, callback: (newValue: U) => void) {
+    this.subscribers.set(callback, {
+      callback,
+      currentValue: selectorFn.call(null, this.internalData),
+      selectorFn,
+    });
+
+    return () => this.unsubscribe(callback);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  unsubscribe(callback: (newValue: any) => void) {
+    this.subscribers.delete(callback);
+  }
+
+  useSelector<U>(selectorFn: SelectorFn<T, U>) {
+    const initialValue = selectorFn(this.internalData);
+    const resubscribing = useRef(false);
+    const [state, setState] = useState<U>(initialValue);
+
+    useEffect(() => {
+      if (resubscribing.current) {
+        setState(initialValue);
+      }
+      resubscribing.current = true;
+
+      const callback = (newValue: U) => {
+        setState(newValue);
+      };
+      return this.subscribe(selectorFn, callback);
+    }, [initialValue, selectorFn, state]);
+
+    return state;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  DEBUG_destroy() {
+    clearTimeout(this.publishTimer);
+    for (const callback of this.subscribers.keys()) {
+      this.subscribers.delete(callback);
+    }
+  }
+}
