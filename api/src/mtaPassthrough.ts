db860ab2da952b8e9a86f43aeb22315d61e94410
Wes Souza <wesleydesouza@me.com>
2019-10-23 10:07:14 -0400

    Add lint and ts scripts to API, fix ts for web

--- api/src/mtaPassthrough.ts
+++ api/src/mtaPassthrough.ts
@@ -6,6 +6,7 @@ import { MtaPathKeys } from './constants';
 
 const { mta } = config;
 const cache: {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [parsedUrl: string]: any;
 } = {};
 
